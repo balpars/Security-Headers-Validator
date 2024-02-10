1b90dcf75a56a90ab552847eb2232c8ec4bf56c2
balpars <89816332+balpars@users.noreply.github.com>
2024-02-10 16:00:47 +0300

    modified headers to include description

--- headers/x_content_type_options.py
+++ headers/x_content_type_options.py
@@ -1,9 +1,12 @@
 def get_info():
     return info_dict
 
+vuln_str = "Bu header'ın bu şekilde kullanımı şu zafiyeti doğurur"
+
 # priority'yi şimdilik kafama göre yazdım :)
 info_dict = {
     'tite':'X-Content-Type-Options',
     'best-practice': ['nosniff'],
-    'priority': 1
+    'priority': 1,
+    'description': vuln_str
 }
--- headers/x_xss_protection.py
+++ headers/x_xss_protection.py
@@ -1,9 +1,12 @@
 def get_info():
     return info_dict
 
+vuln_str = "Bu header'ın bu şekilde kullanımı şu zafiyeti doğurur"
+
 # priority'yi şimdilik kafama göre yazdım :)
 info_dict = {
     'tite':'X-XSS-Protection',
     'best-practice': [0, None],
     'priority': 2 
+    'description': vuln_str
 }
