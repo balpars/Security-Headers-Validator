99ecf126bf3758accab33dcb14c389dafd33c328
balpars <89816332+balpars@users.noreply.github.com>
2024-02-10 15:59:36 +0300

    modified headers to include description

--- headers/header_template.py
+++ headers/header_template.py
@@ -1,12 +1,13 @@
 def get_info():
     return info_dict
 
-vuln = "Bu header'ın kullanımı şu zafiyetleri doğurur"
+vuln_str = "Bu header'ın bu şekilde kullanımı şu zafiyeti doğurur"
 
 # var olmaması lazımsa (eski bir header gibi) best practice'e None diyelim
 # priority'yi şimdilik kafama göre yazdım :)
 info_dict = {
     'tite':'sample_header',
     'best-practice': [None], 
-    'priority': 1
+    'priority': 1,
+    'description': vuln_str
 }
