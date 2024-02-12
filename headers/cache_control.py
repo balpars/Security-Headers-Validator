844cbb506ca08b9e0be29a65f16b93edc1a30ee0
balpars <89816332+balpars@users.noreply.github.com>
2024-02-12 19:35:53 +0300

    added some headers

--- headers/cache_control.py
+++ headers/cache_control.py
@@ -3,12 +3,12 @@ def get_info():
 
 
 desc_str = "Bu header'in bu sekilde kullanimi bu zafiyete sebep olur, su sekilde ayarlanmalidir"
-best_practice_str = "1"
+best_practice_str = "no-store, max-age=0"
 
 info_dict = {
-    'title': 'X-XSS-Protection',
+    'title': 'Sample-Header',
     'best-practice': best_practice_str,
-    'priority': "1",
+    'priority': "1", # 2 if it's not a security header, but is used to hide server info
     'description': desc_str,
-    'deprecated': "1"
+    'deprecated': "1" # 1 if it should no longer be used
 }
--- headers/template_header.py
+++ headers/template_header.py
@@ -1,13 +1,14 @@
 def get_info():
     return info_dict
 
-vuln_str = "Bu header'in bu sekilde kullanimi bu zafiyete sebep olur, su sekilde ayarlanmalidir"
+
+desc_str = "Bu header'in bu sekilde kullanimi bu zafiyete sebep olur, su sekilde ayarlanmalidir"
 best_practice_str = "1, None"
 
 info_dict = {
-    'title':'Sample-Header',
+    'title': 'Sample-Header',
     'best-practice': best_practice_str,
-    'priority': "1",
-    'description': vuln_str, 
-    'obsolete': "1"
+    'priority': "1", # 2 if it's not a security header, but is used to hide server info
+    'description': desc_str,
+    'deprecated': "1" # 1 if it should no longer be used
 }
