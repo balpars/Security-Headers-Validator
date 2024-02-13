cb8a11c4a7a695ffa3d36f079e60d6e8e37b3076
balpars <89816332+balpars@users.noreply.github.com>
2024-02-13 14:04:46 +0300

    refactored for readability

--- header_comparator.py
+++ header_comparator.py
@@ -16,12 +16,13 @@ def find_missing_headers(local_header_dict, response_header_dict):
         priority = data['priority']
         description = data['description']
         is_obsolete = data['deprecated']
-        
+
         if is_obsolete == "1":
             continue
 
-        missing_header_data_list.append(Data(title=title, best_practice=best_practice, priority=priority, description=description,
-                              is_obsolete=is_obsolete))
+        missing_header_data_list.append(
+            Data(title=title, best_practice=best_practice, priority=priority, description=description,
+                 is_obsolete=is_obsolete))
 
     return missing_header_data_list
 
@@ -34,7 +35,7 @@ def find_deprecated_headers(local_header_dict, response_header_dict):
     common_response_headers = list(local_header_set & response_header_set)
 
     deprecated_response_headers = []
-    
+
     for key in common_response_headers:
         data = local_header_dict[key]
         title = data['title']
@@ -46,10 +47,12 @@ def find_deprecated_headers(local_header_dict, response_header_dict):
         if is_obsolete == "0":
             continue
 
-        deprecated_response_headers.append(Data(title=title, best_practice=best_practice, priority=priority, description=description,
-                                is_obsolete=is_obsolete))
-    
+        deprecated_response_headers.append(
+            Data(title=title, best_practice=best_practice, priority=priority, description=description,
+                 is_obsolete=is_obsolete))
+
     return deprecated_response_headers
+
 
 def find_security_headers(local_header_dict, response_header_dict):
     local_header_key_list = list(local_header_dict.keys())
@@ -66,11 +69,11 @@ def find_security_headers(local_header_dict, response_header_dict):
         priority = data['priority']
         description = data['description']
         is_obsolete = data['deprecated']
-        
+
         if is_obsolete == "1":
             continue
 
         data_list.append(Data(title=title, best_practice=best_practice, priority=priority, description=description,
                               is_obsolete=is_obsolete))
-    
+
     return data_list
--- scanner.py
+++ scanner.py
@@ -15,13 +15,17 @@ def main():
 
     found_security_headers = header_comparator.find_security_headers(local_header_dict, response_header_dict)
     missing_headers = header_comparator.find_missing_headers(local_header_dict, response_header_dict)
-    deprecated_headers= header_comparator.find_deprecated_headers(local_header_dict, response_header_dict)
+    deprecated_headers = header_comparator.find_deprecated_headers(local_header_dict, response_header_dict)
 
-    report_writer.write_report_short(data_list=found_security_headers, desc_str="These are the security headers found in target:", color="green")
+    report_writer.write_report_short(data_list=found_security_headers,
+                                     desc_str="These are the security headers found in target:", color="green")
     print()
-    report_writer.write_report(data_list=missing_headers,desc_str="These are the missing headers in target", color="red")
+    report_writer.write_report(data_list=missing_headers, desc_str="These are the missing headers in target",
+                               color="red")
     print()
-    report_writer.write_report(data_list=deprecated_headers, desc_str= "These headers are deprecated, and should no longer be used", color="yellow")
+    report_writer.write_report(data_list=deprecated_headers,
+                               desc_str="These headers are deprecated, and should no longer be used", color="yellow")
+
 
 if __name__ == "__main__":
     main()
